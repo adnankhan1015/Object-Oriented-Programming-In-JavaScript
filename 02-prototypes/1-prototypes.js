13b8cf09fe025b63e7e7106607e219b68c1782f4
Adnan Khan <Adnan.Khan2@ocean.ibm.com>
2023-01-14 21:10:52 +0500

    Discuss Classical and Prototypical Inheritance

---
+++ 02-prototypes/1-prototypes.js
@@ -0,0 +1,13 @@
+// ! Inheritance
+// * Is one of the core concepts of OOP, that enables an object to take on the properties and methods of another object, and this makes it easy to reuse code in different parts of an application.
+
+class Shape {}
+
+class Circle extends Shape {}
+
+// ! In JS we don't have "Classes" we only have objects.
+
+// So That's when "Prototypical Inheritance" comes in the picture.
+// So essentially we have 2 types of inheritance.
+// * 1. Classical Inheritance
+// * 2. Prototypical Inheritance
